0df922b24ae427c16230a4186deeb6c9a44ecb94
Vicente Banderas <vicente.banderas@duocuc.cl>
2026-09-14 02:51:43 -0300

    feat: credenciales anonimas en S3Client cuando no hay claves (lab sin IAM)

--- src/main/java/com/pasalapeli/movie/config/AwsS3Config.java
+++ src/main/java/com/pasalapeli/movie/config/AwsS3Config.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AnonymousCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
